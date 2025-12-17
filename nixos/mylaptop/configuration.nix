2195e292e05c1551ade109083233cad632753b38
lvdund <lvdund@gmail.com>
2025-12-17 11:31:34 +0700

    separate homepc vs mylaptop fix #2

--- nixos/mylaptop/configuration.nix
+++ nixos/mylaptop/configuration.nix
@@ -10,10 +10,8 @@
 
   networking.hostName = "mylaptop";
 
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-  };
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_6_1;
 
   hardware = {
