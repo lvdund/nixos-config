7eb0977384dc778beeea364a81247992244da49c
lvdund <lvdund@gmail.com>
2025-12-30 09:56:05 +0700

    df

---
+++ rm-dotfile.sh
@@ -0,0 +1,10 @@
+rm ~/.bashrc
+rm ~/.tmux.conf
+rm -rf ~/.tmux
+rm -rf ~/.config/kitty
+rm -rf ~/.config/fish
+rm -rf ~/.config/i3
+rm -rf ~/.config/nvim
+rm -rf ~/.local/state/nvim
+rm -rf ~/.local/share/nvim
+rm -rf ~/.cache/nvim
