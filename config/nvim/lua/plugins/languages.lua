bb41407ccc34dccc710e9f85123cc51850812ad9
lvdund <lvdund@gmail.com>
2026-02-16 11:38:23 +0700

    markdown nvim

--- config/nvim/lua/plugins/languages.lua
+++ config/nvim/lua/plugins/languages.lua
@@ -17,11 +17,9 @@ return {
     },
   },
   {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
+    'OXY2DEV/markview.nvim',
+    lazy = false,
+    dependencies = { "saghen/blink.cmp" },
   },
   {
     'zgs225/gomodifytags.nvim',
