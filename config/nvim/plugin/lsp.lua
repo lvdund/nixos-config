25f9391153f42be7d71c4a37fb04cdfc75038f73
lvdund <lvdund@gmail.com>
2026-05-02 22:05:56 +0700

    clangd + network laptop

--- config/nvim/plugin/lsp.lua
+++ config/nvim/plugin/lsp.lua
@@ -1,22 +1,22 @@
-vim.lsp.enable({ "gopls", "lua_ls" })
+vim.lsp.enable({ "gopls", "lua_ls", "clangd" })
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("my.lsp", { clear = true }),
-	callback = function(ev)
-		local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
-		if
-			not client:supports_method("textDocument/willSaveWaitUntil")
-			and client:supports_method("textDocument/formatting")
-		then
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = vim.api.nvim_create_augroup("my.lsp.fmt", { clear = false }),
-				buffer = ev.buf,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
-				end,
-			})
-		end
-	end,
+  group = vim.api.nvim_create_augroup("my.lsp", { clear = true }),
+  callback = function(ev)
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+    if
+        not client:supports_method("textDocument/willSaveWaitUntil")
+        and client:supports_method("textDocument/formatting")
+    then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = vim.api.nvim_create_augroup("my.lsp.fmt", { clear = false }),
+        buffer = ev.buf,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
+        end,
+      })
+    end
+  end,
 })
